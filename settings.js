8c3bc355709725ed8adca71c8213c3e7a2c302de
jiangchao <jiangchao_hubei@163.com>
2018-05-08 17:14:07 +0800

    jc-更改settings

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings={
   timeout:5000,
-  agenda_rest_url:"http://106.75.123.150/api/job",
-  mongodb:"mongodb://root:0o3ei8ujfmc@10.9.107.130:27017/agenda?authSource=admin&authMechanism=SCRAM-SHA-1",
+  agenda_rest_url:"http://localhost/api/job",
+  mongodb:"mongodb://root:root@localhost:27017/agenda",
   port:8080
 }
 
