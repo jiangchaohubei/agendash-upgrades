99b665cde94d9ff96ba0a17b0272c1c5fbafed56
jiangchaohubei <jiangchao_hubei@163.com>
2018-05-08 09:52:30 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,6 +3,8 @@ const settings={
   agenda_rest_url:"http://106.75.123.150/api/job",
   mongodb:"mongodb://root:0o3ei8ujfmc@10.9.107.130:27017/agenda?authSource=admin&authMechanism=SCRAM-SHA-1",
   port:8080
+  
+  
 }
 
 module.exports=settings
