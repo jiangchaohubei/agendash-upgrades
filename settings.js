f1cc9f2d3180b7f71bfefd6f171c8ae8b816db3c
jiangchaohubei <jiangchao_hubei@163.com>
2018-05-10 17:49:22 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const settings={
   timeout:5000,
   agenda_rest_url:"http://localhost/api/job",
   mongodb:"mongodb://root:root@localhost:27017/agenda",
-  port:8081
+  port:8080
   
   
 }
-
+//test
 module.exports=settings
